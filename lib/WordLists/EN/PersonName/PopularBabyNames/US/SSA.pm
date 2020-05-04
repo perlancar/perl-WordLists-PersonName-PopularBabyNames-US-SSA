784cdb4e83ca7b7537aa50dee429e3bb165ce8a6
perlancar (on pc-home) <perlancar@gmail.com>
2020-05-04 15:52:51 +0700

    Rename modules from WordList{,::EN}::PersonName::PopularBabyNames::US::SSA::*, for consistency

---
+++ lib/WordLists/EN/PersonName/PopularBabyNames/US/SSA.pm
@@ -0,0 +1,16 @@
+package WordLists::EN::PersonName::PopularBabyNames::US::SSA;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Popular baby names in the USA (from Social Security Administration)
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<https://www.ssa.gov/oact/babynames/>
